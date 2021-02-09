0a55d55b458a76b63d286a3bc29bddfcd2676c17
Boris Sheludko <bs.sheludko@gmail.com>
2021-02-09 16:03:01 +0300

    changed common info

--- lexsubgen/utils/register.py
+++ lexsubgen/utils/register.py
@@ -1,5 +1,4 @@
 import logging
-import os
 from pathlib import Path
 
 from joblib import Memory
@@ -7,11 +6,12 @@ from joblib import Memory
 logger = logging.getLogger(Path(__file__).name)
 logger.setLevel(logging.INFO)
 
-CACHE_DIR = Path(os.environ["HOME"]) / ".cache" / "lexsubgen"
-DATASETS_DIR = CACHE_DIR / "datasets"
 ENTRY_DIR = Path(__file__).resolve().parent.parent.parent
 CONFIGS_DIR = ENTRY_DIR / "configs"
 LOGS_DIR = ENTRY_DIR / "logs"
 FRONTEND_DIR = ENTRY_DIR / "lex"
+
+CACHE_DIR = Path(os.environ["HOME"]) / ".cache" / "lexsubgen"
+DATASETS_DIR = CACHE_DIR / "datasets"
 MEMORY_CACHE_PATH = CACHE_DIR / "function_cache"
 memory = Memory(str(MEMORY_CACHE_PATH), verbose=0)
